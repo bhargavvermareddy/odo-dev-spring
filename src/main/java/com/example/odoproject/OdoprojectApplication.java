3111c03ddc3c166a28f7eb2594ccb046eba57d9e
Bhargav <bhargavreddy.vermareddy@gmail.com>
2023-02-08 03:02:45 +0000

    initial commit

---
+++ src/main/java/com/example/odoproject/OdoprojectApplication.java
@@ -0,0 +1,13 @@
+package com.example.odoproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OdoprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OdoprojectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/odoproject/OdoprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.odoproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OdoprojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
